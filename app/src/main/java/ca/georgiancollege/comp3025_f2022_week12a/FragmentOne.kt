8dd6faa64e3e260cfe51578fae1a355dfd5a24b0
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-12-02 09:21:33 -0500

    added moving between fragments functionality

--- app/src/main/java/ca/georgiancollege/comp3025_f2022_week12a/FragmentOne.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_f2022_week12a/FragmentOne.kt
@@ -1,6 +1,36 @@
 package ca.georgiancollege.comp3025_f2022_week12a
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
-class FragmentOne: Fragment(R.layout.fragment_one) {
+class FragmentOne: Fragment()
+{
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var FragmentOneToTwoButton = getView()?.findViewById<Button>(R.id.fragment_one_to_two_button)
+
+        FragmentOneToTwoButton?.setOnClickListener{
+            (activity as MainActivity).supportFragmentManager.commit{
+                replace<FragmentTwo>(R.id.fragment_container)
+            }
+        }
+    }
 }
--- app/src/main/java/ca/georgiancollege/comp3025_f2022_week12a/FragmentTwo.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_f2022_week12a/FragmentTwo.kt
@@ -1,6 +1,24 @@
 package ca.georgiancollege.comp3025_f2022_week12a
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
-class FragmentTwo: Fragment(R.layout.fragment_two) {
+class FragmentTwo: Fragment(R.layout.fragment_two)
+{
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        var FragmentTwoToOneButton = getView()?.findViewById<Button>(R.id.fragment_two_to_one_button)
+
+        FragmentTwoToOneButton?.setOnClickListener{
+            (activity as MainActivity).supportFragmentManager.commit{
+                replace<FragmentOne>(R.id.fragment_container)
+            }
+        }
+    }
 }
