c391de0adad323e5122d15021408b01d5e93933f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-12-02 08:59:54 -0500

    added Fragment Switching functionality

--- app/src/main/java/ca/georgiancollege/comp3025_f2022_week12a/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_f2022_week12a/MainActivity.kt
@@ -2,10 +2,29 @@ package ca.georgiancollege.comp3025_f2022_week12a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var FragmentOneButton = findViewById<Button>(R.id.fragment_one_button)
+        var FragmentTwoButton = findViewById<Button>(R.id.fragment_two_button)
+
+        FragmentOneButton.setOnClickListener{
+            supportFragmentManager.commit {
+                replace<FragmentOne>(R.id.fragment_container)
+            }
+        }
+
+        FragmentTwoButton.setOnClickListener {
+            supportFragmentManager.commit{
+                replace<FragmentTwo>(R.id.fragment_container)
+            }
+        }
     }
 }
